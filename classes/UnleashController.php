55ee3200fec7610aaacfd0340a169c9d8031e8e4
Johnnie Clapper <johnnie.clapper@none-records.com>
2021-10-25 22:56:01 +0200

    update splash-screen feature flag with content

--- classes/UnleashController.php
+++ classes/UnleashController.php
@@ -50,13 +50,14 @@ class DotEnv
 
 use DevCoder\DotEnv;
 
-$absolutePathToEnvFile = '/var/www/html/.env';
+$absolutePathToEnvFile = '.env';
 
 (new DotEnv($absolutePathToEnvFile))->load();
 
 ## Namespace for Feature Flags
 namespace Grav\Plugin\Featureflags;
 use Grav\Common\Grav;
+use Grav\Common\Utils;
 
 ## Script that holds and connects feature flags with GitLab
 use Unleash\Client\UnleashBuilder;
@@ -130,13 +131,106 @@ class UnleashController {
         ## Unleash splash-screen feature
         ##
         if ($unleash->isEnabled("splash-screen")) {
-            echo "<html>";
-            echo "<head></head>";
-            echo "<body class=\"page_bg\">";
-            echo "<h1>This is where my Splash-Screen lives a happy life.</h1>";
-            echo "Hello, today is ";
-            echo date('l, F jS, Y'); //other php code here echo "</body>";
-            echo "</html>";
+            // echo "<html>";
+            // echo "<head></head>";
+            // echo "<body class='page_bg' style='background-image: url(splash-screen-wallpaper.jpg);'>";
+            // echo "<h1>This is where my Splash-Screen lives a happy life.</h1>";
+            // echo "Hello, today is ";
+            // echo date('l, F jS, Y'); //other php code here echo "</body>";
+            // echo "</html>";
+            echo '
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                    <style>
+                        body {
+                            margin: 0;
+                            padding: 0;
+                            font-family: sans-serif;
+                            color: white;
+                            overflow: hidden;
+                        }
+                        
+                        .page_bg {
+                            /* background-image: url('. $env['splash_screen']['image'] . '); */
+                        }
+
+                        .splash-wrapper {
+                            position: absolute;
+                            top: 50%;
+                            left: 50%;
+                            transform: translate(-50%, -50%);
+                            width: 100vw;
+                            height: 100vh;
+                            background: linear-gradient(180deg, rgb(10 50 90), rgb(20 10 30));
+                            background-position: center;
+                            background-size: cover;
+                        }
+                        .splash-visuals {
+                            z-index: -1;
+                        }
+                        @keyframes rotate {
+                            0% {transform: translate(-50%, -50%) rotate(0deg);}
+
+                            100% {transform: translate(-50%, -50%) rotate(360deg);}
+                        }
+                        .splash-visuals .circle {
+                            border: 5px rgba(0, 100, 255 , 0.5);
+                            border-style: solid dashed;
+                            width: 500px;
+                            height: 500px;
+                            box-shadow: 0px 0px 50px 0px rgb(0 50 150) inset, 0px 0px 100px 0px rgb(0 50 150);
+                            position: absolute;
+                            left: 50%;
+                            top: 50%;
+                            transform: translate(-50%, -50%);
+                            border-radius: 1000px;
+                            background-image: radial-gradient(rgba(0 50 150 / 0.2), rgba(0 100 255 / 0.3));
+                            animation: rotate 240s linear 0s infinite;
+                        }
+                        .splash-visuals .background {
+                            width: 100vw;
+                            height: 100vh;
+                            position: absolute;
+                            left: 50%;
+                            top: 50%;
+                            transform: translate(-50%, -50%);
+                            background-image: url(tech.jpg);
+                            opacity: 0.1;
+                            background-position: center;
+                            background-size: cover;
+                            filter: grayscale(0.5);
+                        }
+
+                        .splash-content {
+                            text-align: center;
+                            max-width: 400px;
+                            position: absolute;
+                            top: 50%;
+                            left: 50%;
+                            transform: translate(-50%, -50%);
+                        }
+                    </style>
+                </head>
+                <body class="page_bg">
+                    
+                    <div class="splash-wrapper">
+                        <div class="splash-visuals">
+                            <div class="background"></div>
+                            <div class="circle"></div>
+                        </div>
+                        <div class="splash-content">
+                            '. $env['splash_screen']['content'] .'
+                        </div>
+                    </div>
+
+                </body>
+                </html>
+            ';
             die();
           } else {
           }
